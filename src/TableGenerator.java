7a30126fb91d45dba57b35c6a7b2078f89390182
Furqan Shaikh <fshaikh2@harriscomputer.com>
2021-03-13 15:43:48 +0500

    a sample for printing float value using formatted print

--- src/TableGenerator.java
+++ src/TableGenerator.java
@@ -2,6 +2,9 @@ public class TableGenerator {
 
     public static void main(String[] args) {
         int table = 3;
+        float per = 34.98f;
+
+        System.out.printf("The float value is %f %n %n" , per);
 
 //        for (int i = 1; i <= 10; i++) {
 //            String str = table + " x " + i + " = " + table * i;
