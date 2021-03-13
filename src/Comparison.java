4ca93b604193c31d19d2e86d9e17850f3e497819
Furqan Shaikh <fshaikh2@harriscomputer.com>
2021-03-13 15:52:04 +0500

    Comparision Test

---
+++ src/Comparison.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Comparison {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first integer: "); // prompt
+        int number1 = input.nextInt(); // read first number from user
+
+        System.out.print("Enter second integer: "); // prompt
+        int number2 = input.nextInt(); // read second number from user
+
+        if (number1 == number2) {
+            System.out.printf("%d == %d%n", number1, number2);
+        }
+
+        if (number1 != number2) {
+            System.out.printf("%d != %d%n", number1, number2);
+        }
+
+        if (number1 < number2) {
+            System.out.printf("%d < %d%n", number1, number2);
+        }
+
+        if (number1 > number2) {
+            System.out.printf("%d > %d%n", number1, number2);
+        }
+
+        if (number1 <= number2) {
+            System.out.printf("%d <= %d%n", number1, number2);
+        }
+
+        if (number1 >= number2) {
+            System.out.printf("%d >= %d%n", number1, number2);
+        }
+    } // end method main
+} // end class Comparison
+
---
+++ src/TestPrint.java
@@ -0,0 +1,19 @@
+public class TestPrint {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World !");
+        System.out.println("Hello World !");
+        System.out.println("Hello World !");
+        System.out.println("Hello World !");
+        System.out.println("Hello World !");
+        System.out.println("Hello World !");
+        System.out.println("Hello World !");
+        System.out.println("Hello World !");
+        System.out.println("Hello World !");
+        System.out.println("Hello World !");
+        System.out.println("Hello World !");
+        System.out.println("Hello World !"); // ctrl + D to duplicate the line
+
+    }
+}
