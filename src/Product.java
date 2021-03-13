115b1c315f3354e3bbb3fd4d18e3047e35fc3d5a
Furqan Shaikh <fshaikh2@harriscomputer.com>
2021-03-13 15:32:31 +0500

    The product test

---
+++ src/Product.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Product {
+
+    public static void main(String[] args) {
+        // create Scanner to obtain input from command window
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first integer: "); // prompt for input
+        int x = input.nextInt(); // read first integer
+
+        System.out.print("Enter second integer: "); // prompt for input
+        int y = input.nextInt(); // read second integer
+
+        System.out.print("Enter third integer: "); // prompt for input
+        int z = input.nextInt(); // read third integer
+
+        int result = x * y * z; // calculate product of numbers
+
+        System.out.printf("The Product of %d %d %d is %d", x, y, z, result);
+    } // end method main
+
+}
